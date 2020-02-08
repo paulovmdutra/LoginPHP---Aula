646f0e67f02a84bc32c0d6fd3564008d84e9f6a5
Paulo Vinícius <paulovmdutra@gmail.com>
2020-02-08 10:21:09 -0300

    Primeiro commit

---
+++ app/src/main/java/com/tutorialandroidphp/loginphp/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.tutorialandroidphp.loginphp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnRegistrarUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnRegistrarUsuario = (Button)findViewById(R.id.btnRegistrar);
+    }
+
+    public void showRegistrarUsuario(View view){
+
+        Intent it = new Intent(this, RegistrarUsuarioActivity.class);
+        startActivity(it);
+
+    }
+
+}
---
+++ app/src/main/java/com/tutorialandroidphp/loginphp/RegistrarUsuarioActivity.java
@@ -0,0 +1,15 @@
+package com.tutorialandroidphp.loginphp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+
+}
